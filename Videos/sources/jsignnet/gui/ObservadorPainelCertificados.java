13a84340084deec0985db635800a141b984094b2
Alisson Santos • Desenvolvedor FullStack <159386996+devsantosx7@users.noreply.github.com>
2026-05-04 17:48:16 -0300

    Delete Videos/sources/jsignnet directory

--- Videos/sources/jsignnet/gui/ObservadorPainelCertificados.java
+++
@@ -1,8 +0,0 @@
-package jsignnet.gui;
-
-import jsignnet.crypto.X509CertificadoWrapper;
-
-/* loaded from: jsignnet-3.6.0.jar:jsignnet/gui/ObservadorPainelCertificados.class */
-public interface ObservadorPainelCertificados {
-    void certificadoSelecionado(PainelCertificados painelCertificados, X509CertificadoWrapper x509CertificadoWrapper);
-}
--- Videos/sources/jsignnet/infra/IFiltro.java
+++
@@ -1,8 +0,0 @@
-package jsignnet.infra;
-
-import jsignnet.crypto.X509CertificadoWrapper;
-
-/* loaded from: jsignnet-3.6.0.jar:jsignnet/infra/IFiltro.class */
-public interface IFiltro {
-    boolean isCertificadoValido(X509CertificadoWrapper x509CertificadoWrapper);
-}
